76086b997eec5babf7297dea700a12e861a3f649
fahchen <06fahchen@gmail.com>
2015-09-11 12:20:25 +0800

    Question has many choices

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   validates :title, presence: true
 
   belongs_to :poll
+  has_many :choices
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911041709) do
+ActiveRecord::Schema.define(version: 20150911041857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "choices", force: :cascade do |t|
     t.string   "title"
-    t.integer  "limit",      default: 0
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "limit",       default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "question_id"
   end
+
+  add_index "choices", ["question_id"], name: "index_choices_on_question_id", using: :btree
 
   create_table "polls", force: :cascade do |t|
     t.string   "title"
@@ -50,5 +53,6 @@ ActiveRecord::Schema.define(version: 20150911041709) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "choices", "questions"
   add_foreign_key "questions", "polls"
 end

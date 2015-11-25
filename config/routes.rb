e8869c3c54bd617832cde97db1df584e804ebea6
Breeza <cbasicok@gmail.com>
2015-11-25 21:44:49 +0800

    Add fill to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :polls, only: [:new, :create, :show, :update, :destroy]
+  resources :polls, only: [:new, :create, :show, :update, :destroy] do
+    member do
+      get :fill
+    end
+  end
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
